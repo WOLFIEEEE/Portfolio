1b6662b3e2ea74fdcf765ebe63248576f3b5f2f5
WOFLIEEEE <kgpkhushwant1@gmail.com>
2023-06-11 01:16:11 +0530

    Major Changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,9 @@ module.exports = {
     title: 'Khushwant Parihar',
     description:
       'Khushwant Parihar is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    siteUrl: 'https://khushwantparihar.in', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@wolfiekhushwant',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
